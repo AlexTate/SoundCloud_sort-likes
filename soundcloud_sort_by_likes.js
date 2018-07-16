228c9cfc2fc7b1e4e5c3b6e88d79efe933edbdc9
Alex Tate <0xalextate@gmail.com>
2018-07-15 22:39:07 -0600

    Update soundcloud_sort_by_likes.js

--- soundcloud_sort_by_likes.js
+++ soundcloud_sort_by_likes.js
@@ -1,8 +1,8 @@
 (function(){
-	var d = document.querySelectorAll(".soundList__item");
-	var stream = d[0].parentNode;
+	var posts = document.querySelectorAll(".soundList__item");
+	var stream = posts[0].parentNode;
 
-	var sorted = Array.from(d).sort(function(a, b){	
+	var sorted = Array.from(posts).sort(function(a, b){	
 		var aLike = parseInt(a.querySelector("[class^=sc-button-like]").innerText) || 0;
 		var bLike = parseInt(b.querySelector("[class^=sc-button-like]").innerText) || 0;
 
