5a11cd1494d7afcf78cb1575e10a5b1293606dfb
Alex Tate <0xalextate@gmail.com>
2018-09-05 11:44:01 -0600

    Special cases handled
    Previous version was a very early prototype and I hadn't realized that my local bookmark no longer matched the code here. This more effectively parses post like counts

--- soundcloud_sort_by_likes.js
+++ soundcloud_sort_by_likes.js
@@ -1,15 +1,15 @@
 (function(){
-	var posts = document.querySelectorAll(".soundList__item");
-	var stream = posts[0].parentNode;
+	var d = document.querySelectorAll(".soundList__item");
+	var stream = d[0].parentNode;
 
-	var sorted = Array.from(posts).sort(function(a, b){	
-		var aLike = parseInt(a.querySelector("[class^=sc-button-like]").innerText) || 0;
-		var bLike = parseInt(b.querySelector("[class^=sc-button-like]").innerText) || 0;
+	var sorted = Array.from(d).sort(function(a, b) {
+	    var aLike = parseInt(a.querySelector("[class^=sc-button-like]").innerText.match(/(\d)/g).join("")) || 0;
+	    var bLike = parseInt(b.querySelector("[class^=sc-button-like]").innerText.match(/(\d)/g).join("")) || 0;
 
-		return bLike - aLike;
+	    return bLike - aLike;
 	});
 
 	for (let item of sorted) {
-		stream.appendChild(item);
+	    stream.appendChild(item);
 	}
 })();
